34351359cb27d23a52d2278e61f3829aa88804d4
ConstantineF <warsawxx@gmail.com>
2023-02-24 03:08:04 +0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import Card from "./components/Card";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const dfwJSON = await fetch("./src/dfw.json");
+      const data = await dfwJSON.json();
+      const numRandom = Math.floor(Math.random() * data.length);
+      const wordRandom = data[numRandom].word.slice(0, -1);
+      const descriptionRandom = data[numRandom].description;
+      setWord(wordRandom);
+      setDescription(descriptionRandom);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <Card
-        word="Matriculation"
-        definition="Formal process of entering a university"
-      />
+      <h1>Random rare word from Infinite Jest by DFW</h1>
+
+      <Card word={word} definition={description} />
       <img
         src="./src/images/infinite_jest.webp"
         alt="A white cloud against a red sky."
